5b31ef62522198f8db64cab1f053d4d23f37aeaf
JarekJanota <jarek.janota8@gmail.com>
2023-10-24 19:27:27 +0200

    Dwa wykresy, data.json poza folderem

---
+++ main.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import json
+from colorama import Fore
+
+#odczyt json i wyświetlenie w konsoli danych
+df = pd.read_json(r'C:\Users\jaros\Desktop\data.json')
+print(df.to_string())
+
+#wyświetlenie maksymalnego pulsu
+max_pulse = df["Maxpulse"].max()
+print(f"Maksymalny puls (Maxpulse): {max_pulse}")
+
+#wykres 1
+ax = df.plot(y=["Maxpulse"], kind="bar", color="red")
+df.plot(kind='bar', y=["Pulse"], color='green', ax=ax, title='maksymalny/aktualny puls')
+
+#wykres 2
+df.plot(kind='bar', y=["Pulse", "Maxpulse"], cmap='cividis', title='maksymalny/aktualny puls')
+
+#zakres osi Y do maxa + 10 oczek wyżej, przeskok co 20 oczek
+plt.yticks(range(0, df["Maxpulse"].max() + 10, 20))
+ax.set_yticks(range(0, df[["Pulse", "Maxpulse"]].max().max() + 10, 20))
+
+# Obrócenie etykiet na pionowo
+ax.set_xticklabels(ax.get_xticklabels(), rotation=0)
+plt.xticks(rotation='horizontal')
+
+#wyświetlenie wykresów
+plt.show()
